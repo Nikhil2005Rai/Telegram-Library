2770cb9a01625195a7387bd4b58c3cae441c78c7
Nikhil2005Rai <n2005rai@gmail.com>
2026-06-06 15:13:02 +0530

    fix: multirun fix

--- stringSession.py
+++ stringSession.py
@@ -1,8 +1,23 @@
+
+
+from pathlib import Path
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-api_id=38710207
-api_hash="0d1f84b65e2ad492d40059f6b017ec78"
+api_id = 38710207
+api_hash = "0d1f84b65e2ad492d40059f6b017ec78"
 
-with TelegramClient("sessions/session", api_id, api_hash) as client:
+BASE_DIR = Path(__file__).resolve().parent
+
+SESSION_FILE = (
+    BASE_DIR
+    / "sessions"
+    / "session"
+)
+
+with TelegramClient(
+    str(SESSION_FILE),
+    api_id,
+    api_hash
+) as client:
     print(StringSession.save(client.session))
